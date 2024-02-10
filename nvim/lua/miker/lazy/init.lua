a9a67bfd5b211245fdbe9cd982e89a320d2475bc
Mike Rosseel <mike.rosseel@gmail.com>
2024-02-10 23:37:06 +0100

    add comments

--- nvim/lua/miker/lazy/init.lua
+++ nvim/lua/miker/lazy/init.lua
@@ -1,4 +1,11 @@
 return {
 	{ "nvim-lua/plenary.nvim" },
-    { "github/copilot.vim" },
+  { "github/copilot.vim" },
+  {
+      'numToStr/Comment.nvim',
+      opts = {
+          -- add any options here
+      },
+      lazy = false,
+  }
 }
