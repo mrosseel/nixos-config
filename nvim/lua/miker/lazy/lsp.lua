7f06dbe741ebd6576784a7019c9d412915635e00
Mike Rosseel <mike.rosseel@gmail.com>
2024-02-17 14:14:48 +0100

    Fix cmp not inserting text

--- nvim/lua/miker/lazy/lsp.lua
+++ nvim/lua/miker/lazy/lsp.lua
@@ -51,12 +51,13 @@ return {
                         require('luasnip').lsp_expand(args.body)
                     end,
                 },
-                mapping = cmp.mapping.preset.insert({
-                  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                  ["<C-Space>"] = cm
-              }),
+                 mapping = cmp.mapping.preset.insert({
+                  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                  ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                  ['<C-Space>'] = cmp.mapping.complete(),
+                  ['<C-e>'] = cmp.mapping.abort(),
+                  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                }),
                 sources = {
                     { name = 'nvim_lsp' },
                     { name = 'buffer' },
