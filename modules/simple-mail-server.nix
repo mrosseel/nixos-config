d142d10196dd22be04fad1155301aa2b4f2145dc
Mike Rosseel <mike.rosseel@gmail.com>
2024-05-20 19:44:13 +0200

    Fix website, start email server

---
+++ modules/simple-mail-server.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }: {
+  imports = [
+    (builtins.fetchTarball {
+      # Pick a release version you are interested in and set its hash, e.g.
+      url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/nixos-23.05/nixos-mailserver-nixos-23.05.tar.gz";
+      # To get the sha256 of the nixos-mailserver tarball, we can use the nix-prefetch-url command:
+      # release="nixos-23.05"; nix-prefetch-url "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/${release}/nixos-mailserver-${release}.tar.gz" --unpack
+      sha256 = "1ngil2shzkf61qxiqw11awyl81cr7ks2kv3r3k243zz7v2xakm5c";
+    })
+  ];
+
+  mailserver = {
+    enable = true;
+    fqdn = "mail.pifinder.eu";
+    domains = [ "pifinder.eu" ];
+
+    # A list of all login accounts. To create the password hashes, use
+    # nix-shell -p mkpasswd --run 'mkpasswd -sm bcrypt'
+    loginAccounts = {
+      "info@pifinder.eu" = {
+        hashedPassword = "$2b$05$WvFx1wGOLhX5mLyWCPPVLebGTzjCF6J6TUl.9ejxkC5mHEw83DVqy";
+        aliases = ["postmaster@pifinder.eu"];
+      };
+    };
+
+    # Use Let's Encrypt certificates. Note that this needs to set up a stripped
+    # down nginx and opens port 80.
+    certificateScheme = "acme-nginx";
+  };
+  security.acme.acceptTerms = true;
+  security.acme.defaults.email = "security@pifinder.eu";
+}
+
