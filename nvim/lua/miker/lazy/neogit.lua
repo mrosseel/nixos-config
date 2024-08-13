7b10f8aa860c074918f2999e0080311182745aea
Mike Rosseel <mike.rosseel@gmail.com>
2024-08-13 12:16:24 +0200

    Activate telescope and diffview integration

--- nvim/lua/miker/lazy/neogit.lua
+++ nvim/lua/miker/lazy/neogit.lua
@@ -152,12 +152,12 @@ return {
       integrations = {
         -- If enabled, use telescope for menu selection rather than vim.ui.select.
         -- Allows multi-select and some things that vim.ui.select doesn't.
-        telescope = nil,
+        telescope = true,
         -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
         -- The diffview integration enables the diff popup.
         --
         -- Requires you to have `sindrets/diffview.nvim` installed.
-        diffview = nil,
+        diffview = true,
 
         -- If enabled, uses fzf-lua for menu selection. If the telescope integration
         -- is also selected then telescope is used instead
